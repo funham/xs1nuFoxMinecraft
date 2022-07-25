105bf306fc2341181ff6d1bf9efd70e0cb8dc110
xs1nus <fun1ham@gmail.com>
2022-07-25 12:58:32 +0300

    saved

--- run.bat
+++ run.bat
@@ -1,6 +1,1 @@
-git fetch
-git pull
-java -Xms6G -Xmx6G -jar server.jar nogui
-git add .
-git commit -m "saved"
-git push
+python script.py
---
+++ script.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+os.system('git fetch')
+os.system('git pull')
+
+with open('lock.txt') as file:
+    status = file.read()
+
+if status == 'locked':
+    print('server is Locked!')
+    sys.exit()
+
+with open('lock.txt', 'w') as file:
+    file.write('locked')
+
+os.system('git add .')
+os.system('git push')
+
+os.system('java -Xms6G -Xmx6G -jar server.jar nogui')
+
+with open('lock.txt', 'w') as file:
+    file.write('unlocked')
+
+os.system('git add .')
+os.system('git commit -m "saved"')
+os.system('git push')
